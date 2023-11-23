005bcb35e8e5f934ed2fa83423a17620bfa1af30
Shtazzo <emanuele.donno@gmail.com>
2023-11-23 16:03:36 +0100

    - ED :  Added documentation to the code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # 					Copyright 2023 - CMCC Foundation						#
 #																			#
@@ -19,7 +18,6 @@
 # See the License for the specific language governing permissions and		#
 # limitations under the License.											#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-"""
 
 import os
 import joblib
