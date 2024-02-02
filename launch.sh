5b177c05d931c29ad7d6522d01599ff025393288
Emanuele Donno <49711942+Shtazzo@users.noreply.github.com>
2024-02-02 10:52:10 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#BSUB -n 1 							# n_nodes
-#BSUB -P R000						# project
+#BSUB -n 4 							# n processes
 #BSUB -q g_medium					# queue
+#BSUB -P R000						# project
+#BSUB -R "span[ptile=2]"			# n gpu per node
 #BSUB -J wildfires					# jobname
 #BSUB -o job-%J.out
 #BSUB -e job-%J.err
-#BSUB -x 
 #BSUB -gpu "num=2"
-
+#BSUB -x 
 
 #Command to execute Python program
 python3 main.py
