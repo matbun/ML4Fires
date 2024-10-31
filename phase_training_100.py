b6da2efbcb993cc526912ddf0ffa94145f938a9c
Emanuele Donno <emanuele.donno@cmcc.it>
2024-10-31 17:46:19 +0100

    - ED: Major changes, Modified Phase Training file

--- phase_training_100.py
+++ phase_training_100.py
@@ -1,9 +1,13 @@
 import os
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
 import toml
 import xarray as xr
 import numpy as np
 from datetime import datetime as dt
+
+# Settings the warnings to be ignored 
+import warnings
+warnings.filterwarnings('ignore')
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -12,7 +16,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import mlflow
 
 # Itwinai imports
-from itwinai.loggers import MLFlowLogger as MLF_Logger, Prov4MLLogger
+from itwinai.loggers import MLFlowLogger as Itwinai_MLFLogger, Prov4MLLogger
 
 # Pytorch imports
 import torch
@@ -25,13 +29,11 @@ from torchmetrics.classification import Precision, Recall, F1Score, FBetaScore, 
 
 # Lightning imports
 import lightning as L
-import lightning.pytorch as pl
-from lightning.pytorch.loggers import MLFlowLogger
-from lightning.pytorch.callbacks import EarlyStopping
-from lightning.fabric.loggers import CSVLogger
+import lightning.pytorch as lp
 from lightning.fabric.strategies.fsdp import FSDPStrategy
 from lightning.fabric.plugins.environments import MPIEnvironment
 from lightning.pytorch.utilities.rank_zero import rank_zero_only
+
 
 # ML4Fires imports
 import Fires
@@ -39,13 +41,12 @@ from Fires._datasets.torch_dataset import FireDataset
 from Fires._macros.macros import (
 	CONFIG,
 	DRIVERS as drivers,
+	SEED,
 	TARGETS as targets,
 	TRN_YEARS as trn_years,
 	VAL_YEARS as val_years,
 	DATA_PATH_100KM,
 	TORCH_CFG,
-	DISCORD_CFG,
-	CHECKPOINTS_DIR,
 	CREDENTIALS_CFG,
 	DATA_DIR,
 	LOGS_DIR,
@@ -55,69 +56,24 @@ from Fires._macros.macros import (
 )
 import Fires._models
 from Fires._models.unet import Unet
+from Fires._models.unetpp import UnetPlusPlus
 import Fires._models.unetpp
 from Fires._scalers.scaling_maps import StandardMapsPointWise, MinMaxMapsPointWise
 from Fires._scalers.standard import StandardScaler
 from Fires._scalers.minmax import MinMaxScaler
-from Fires._utilities.callbacks import DiscordBenchmark, FabricBenchmark, FabricCheckpoint
 from Fires._utilities.cli_args_checker import checker
 from Fires._utilities.cli_args_parser import CLIParser
 from Fires._utilities.configuration import load_global_config
 from Fires._utilities.decorators import debug
 from Fires._utilities.logger import Logger as logger
-from Fires._utilities.logger_itwinai import ItwinaiLightningLogger
 from Fires._utilities.metrics import TverskyLoss, FocalLoss
+from Fires._utilities.utils_general import check_backend
+from Fires._utilities.utils_mlflow import setup_mlflow_experiment
+from Fires._utilities.utils_trainer import get_callbacks, get_loggers   
 from Fires.trainer import FabricTrainer
 
 # define logger
 _log = logger(log_dir=LOGS_DIR).get_logger("Training_on_100km")
-
-# define environment variables
-os.environ['MLFLOW_TRACKING_INSECURE_TLS'] = 'true'
-os.environ['MLFLOW_TRACKING_USERNAME'] = CREDENTIALS_CFG.credentials.username
-os.environ['MLFLOW_TRACKING_PASSWORD'] = CREDENTIALS_CFG.credentials.password
-TRACKING_URI = 'https://mlflow.intertwin.fedcloud.eu/'
-
-
-def setup_mlflow_experiment():
-	"""
-	Configures MLflow tracking URI and sets the experiment name.
-	This function should be called once at the beginning of the script.
-	"""
-	mlflow.set_tracking_uri(TRACKING_URI)
-	experiment_name = "ML4Fires_LOCAL"
-	mlflow.set_experiment(experiment_name)
-	_log.info(f"MLflow Experiment set to '{experiment_name}' with tracking URI '{TRACKING_URI}'")
-
-
-@debug(log=_log)
-def check_backend() -> str:
-	"""
-	Determines the available backend engine for PyTorch computations.
-
-	This function checks if the MPS (Metal Performance Shaders) or CUDA backends
-	are available and sets the appropriate backend accordingly. If neither MPS 
-	nor CUDA is available, it defaults to the CPU backend.
-
-	Returns
-	-------
-	str
-		The name of the backend to use for PyTorch computations ('mps', 'cuda', or 'cpu').
-	"""
-
-	if torch.backends.mps.is_available():
-		backend:str = 'mps'
-	elif torch.cuda.is_available():
-		backend:str = 'cuda'
-	else:
-		backend:str = 'cpu'
-	
-	if backend in ['mps', 'cuda']:
-		matmul_precision = TORCH_CFG.base.matmul_precision
-		torch.set_float32_matmul_precision(matmul_precision)
-
-	_log.info(f" | {backend.upper()} available")
-	return backend
 
 
 @debug(log=_log)
@@ -125,47 +81,15 @@ def init_fabric():
 	# check backend if MPS, CUDA or CPU
 	backend = check_backend()
 
-	# # get MLFlow logger
-	# mlf_logger = MLFlowLogger(
-	# 	experiment_name="ML4Fires_Juno",
-	# 	tracking_uri=TRACKING_URI,
-	# 	log_model=True,
-	# )
+	# get loggers for Fabric Trainer
+	_loggers = get_loggers()
 
-	# define today's date
-	today = eval(CONFIG.utils.datetime.today)
-	_log.info(f" | Today: {today}")
-
-	# define csv log name
-	csv_fname = f'{today}_csv_logs'
-	_log.info(f" | CSV Filename: {csv_fname}")
-
-
-	# define Itwinai Logger 
-	_itwinai_logger = ItwinaiLightningLogger(savedir=os.path.join(LOGS_DIR, "ITWINAI"))
-	# define Itwinai MLFlow logger
-	_itwinai_mlflow_logger = MLF_Logger(experiment_name=run_name, tracking_uri=TRACKING_URI, log_freq=10)
-	# define CSV logger
-	_csv_logger = CSVLogger(root_dir=RUN_DIR, name=csv_fname)
-	# define Provenance logger
-	_provenance_logger = Prov4MLLogger(experiment_name=run_name, provenance_save_dir=os.path.join(LOGS_DIR, 'prov_logs'), save_after_n_logs=1)
-	# define loggers for Fabric trainer
-	_loggers = [_csv_logger, _itwinai_logger, _itwinai_mlflow_logger, _provenance_logger]
-
-	# define Discord benchmark callback
-	_discord_bench_cllbk = DiscordBenchmark(webhook_url=DISCORD_CFG.hooks.webhook_gen, benchmark_csv=os.path.join(RUN_DIR, "fabric_benchmark.csv"))
-	# define Fabric benchmark callback
-	_fabric_bench_cllbk = FabricBenchmark(filename=os.path.join(RUN_DIR, "fabric_benchmark.csv"))
-	# define Fabric checkpoint callback
-	_fabric_check_cllbk = FabricCheckpoint(dst=CHECKPOINTS_DIR)
-	# define Early Stopping callback
-	_earlystop_cllbk = EarlyStopping('val_loss')
-	# define callbacks for Fabric trainer
-	_callbacks = [_discord_bench_cllbk, _fabric_bench_cllbk, _fabric_check_cllbk, _earlystop_cllbk]
-
+	# get callbacks for Fabric Trainer
+	_callbacks = get_callbacks()
+	
 	# fabric args
 	fabric_args = dict(
-		accelerator=backend,
+		accelerator='gpu' if backend in ['mps', 'cuda'] else 'cpu',
 		strategy=eval(TORCH_CFG.model.strategy) if backend in ['mps', 'cuda'] else 'auto',
 		devices=TORCH_CFG.trainer.devices,
 		num_nodes=TORCH_CFG.trainer.num_nodes,
@@ -271,9 +195,8 @@ def create_torch_datasets(data_source_path:str) -> Tuple[FireDataset, FireDatase
 	return trn_torch_ds, val_torch_ds
 
 
-
 @debug(log=_log)
-def setup_model() -> Unet:
+def setup_model() -> Optional[Unet | UnetPlusPlus]:
 	"""
 	Initializes and configures the UNet model for training.
 
@@ -293,31 +216,72 @@ def setup_model() -> Unet:
 	model.loss = torch.nn.modules.loss.BCELoss()
 	# model.loss = TverskyLoss(alpha=0.5, beta=0.5)
 
-	# define model metrics
+	# define metrics list
+	_metrics = []
+
+	# precision
 	precision = Precision(task='binary')
 	precision.name = "precision"
+	_metrics.append(precision)
 
+	# recall
 	recall = Recall(task='binary')
 	recall.name = "recall"
+	_metrics.append(recall)
 
+	# f1 score
 	f1_score = F1Score(task='binary')
 	f1_score.name = "f1_score"
+	_metrics.append(f1_score)
 
+	# f2 score
 	f2_score = FBetaScore(task='binary', beta=float(2))
 	f2_score.name = "f2_score"
+	_metrics.append(f2_score)
 
+	# mcc
 	mcc = MatthewsCorrCoef(task='binary')
 	mcc.name = "mcc"
+	_metrics.append(mcc)
 
 	all_metrics = False
 
 	# define model metrics
-	model.metrics = [precision, recall, f1_score, f2_score, mcc] if all_metrics else []
+	model.metrics = _metrics if all_metrics else []
 
 	_log.info(f" | Model: \n\n {model}")
 	
 	return model
 
+
+@debug(log=_log)
+def get_lightning_trainer():
+
+	# check backend
+	backend=check_backend()
+
+	# get loggers for Fabric Trainer
+	_loggers = get_loggers(run_name=run_name)
+
+	# get callbacks for Fabric Trainer
+	_callbacks = get_callbacks()
+
+	# seed everything for reproducibility
+	lp.seed_everything(seed=SEED, workers=True)
+
+	# define lightining.pytorch.Trainer object
+	pl_trainer=lp.Trainer(
+		accelerator='gpu' if backend in ['mps', 'cuda'] else 'cpu',
+		strategy='ddp' if backend in ['mps', 'cuda'] else 'auto', # eval(TORCH_CFG.model.strategy) if backend in ['mps', 'cuda'] else 'auto',
+		devices=1, #TORCH_CFG.trainer.devices,
+		num_nodes=1, #TORCH_CFG.trainer.num_nodes,
+		precision= TORCH_CFG.trainer.precision,
+		logger=_loggers,
+		callbacks=_callbacks,
+		max_epochs=5, # TORCH_CFG.trainer.epochs,
+	)
+
+	return pl_trainer
 
 
 @debug(log=_log)
@@ -338,31 +302,33 @@ def main():
 	model = setup_model()
 
 	# load dataloader
-	dloader_args = dict(batch_size=TORCH_CFG.trainer.batch_size, shuffle=True, drop_last=TORCH_CFG.trainer.drop_reminder)
+	dloader_args = dict(batch_size=TORCH_CFG.trainer.batch_size, shuffle=True, drop_last=TORCH_CFG.trainer.drop_reminder, num_workers=2)
 	train_loader = DataLoader(trn_torch_ds,	**dloader_args)
 	valid_loader = DataLoader(val_torch_ds, **dloader_args)
 
 	# get fabric
 	# fabric = init_fabric()
-	fabric_args = init_fabric()
+	# fabric_args = init_fabric()
 
 	# define trainer
-	trainer = get_trainer(fabric_args=fabric_args)
+	# trainer = get_trainer(fabric_args=fabric_args)
 	
 	# setup the model and the optimizer
-	trainer.setup(
-		model=model,
-		optimizer_cls=eval(TORCH_CFG.trainer.optim.cls),
-		optimizer_args=eval(TORCH_CFG.trainer.optim.args),
-		scheduler_cls=eval(TORCH_CFG.trainer.scheduler.cls),
-		scheduler_args=eval(TORCH_CFG.trainer.scheduler.args),
-		checkpoint=eval(TORCH_CFG.trainer.checkpoint.ckpt)
-	)
+	# trainer.setup(
+	# 	model=model,
+	# 	optimizer_cls=eval(TORCH_CFG.trainer.optim.cls),
+	# 	optimizer_args=eval(TORCH_CFG.trainer.optim.args),
+	# 	scheduler_cls=eval(TORCH_CFG.trainer.scheduler.cls),
+	# 	scheduler_args=eval(TORCH_CFG.trainer.scheduler.args),
+	# 	checkpoint=eval(TORCH_CFG.trainer.checkpoint.ckpt)
+	# )
+
+	# get instance of Pytorch Lightning Trainer
+	trainer = get_lightning_trainer()
 
 	# get global rank
 	global_rank = trainer.global_rank
 	print(f" | Global rank {global_rank}")
-	print(f" | Trainer global rank {trainer.global_rank}")
 
 	# Automatically log params, metrics, and model
 	mlflow.pytorch.autolog()
@@ -372,31 +338,38 @@ def main():
 		mlflow.start_run(run_name=run_name)
 
 	# fit the model
-	trainer.fit(train_loader=train_loader, val_loader=valid_loader, log_mlflow=True)
+	trainer.fit(
+		model=model,
+		train_dataloaders=train_loader, 
+		val_dataloaders=valid_loader
+	)
+		# log_mlflow=True)
 
 	# save the model to disk
 	last_model = os.path.join(RUN_DIR,'last_model.pt')
-	trainer.fabric.save(
-		path=last_model,
-		state={
-			'model':trainer.model,
-			'optimizer':trainer.optimizer,
-			'scheduler': trainer.scheduler_cfg
-		}
-	)
+	trainer.save_checkpoint(filepath=last_model)
+
+	# trainer.fabric.save(
+	# 	path=last_model,
+	# 	state={
+	# 		'model':trainer.model,
+	# 		'optimizer':trainer.optimizer,
+	# 		'scheduler': trainer.scheduler_cfg
+	# 	}
+	# )
 
 	# log weights
 	if global_rank == 0:
 		mlflow.log_artifact(last_model, artifact_path="model_weights")
 
 	# log model
-	original_model = trainer.model.module
+	original_model = trainer.model # trainer.model.module
 
 	# Remove non-serializable attributes
-	if hasattr(original_model, '_fabric'):
-		del original_model._fabric
-	if hasattr(original_model, 'comm'):
-		del original_model.comm
+	# if hasattr(original_model, '_fabric'):
+	# 	del original_model._fabric
+	# if hasattr(original_model, 'comm'):
+	# 	del original_model.comm
 
 	original_model.cpu()
 
@@ -407,7 +380,6 @@ def main():
 	# end MLFlow run
 	if global_rank == 0:
 		mlflow.end_run()
-
 
 
 @debug(log=_log)
@@ -466,7 +438,6 @@ def check_cli_args():
 	run_name=f"LOCAL_{cli_model_name.upper()}_BCE_{cli_base_filter_dim}"
 		
 	return model_class, model_config
-
 
 
 if __name__ == '__main__':
